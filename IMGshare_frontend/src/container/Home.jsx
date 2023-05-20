8a534fadd3ca1cc99983095f6fa670b7689ee26e
apoorvsingh2314 <apoorvsingh2314@gmail.com>
2023-05-21 01:54:31 +0530

    Home page changes

--- IMGshare_frontend/src/container/Home.jsx
+++ IMGshare_frontend/src/container/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import {React,useState,useEffect} from 'react';
 
+
+import Sidebar from '../components/Sidebar';
+import UserProfile from '../components/UserProfile';
+import {client} from '../client';
+import Pins from './Pins';
+import { userQuery } from '../utils/data';
 
 const Home = () => {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState();
+
+  const userInfo = localStorage.getItem('user') !== 'undefined' ?  JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId);
+
+    client.fetch(query).then((data) =>{
+      setUser(data[0]);
+    });
+  }, []);
+  
+
   return (
-    <div>
+    <div className='flex bg-slate-600 md:flex-row flex-col h-screen ease-out duration-75'>
+      <div className='hidden md:flex flex-initial'>
+      <Sidebar/>
+      </div>
+      <div className='flex md:hidden flex-row'>
+
+      </div>
+      <UserProfile/>
+      <Pins/>
       Home
     </div>
   );
